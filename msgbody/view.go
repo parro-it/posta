5e8784ed8ec0f021b040a0a4f4bd69aa4447cbd7
Andrea Parodi <andrea@parro.it>
2022-01-21 16:48:59 +0100

    refactor

--- msgbody/view.go
+++ msgbody/view.go
@@ -7,86 +7,114 @@ import (
 	"github.com/parro-it/posta/errs"
 )
 
+type flds struct {
+	from *gtk.Entry
+	to   *gtk.Entry
+	cc   *gtk.Entry
+	subj *gtk.Entry
+	body *gtk.TextView
+}
+
 // Creates a tree view and the tree store that holds its data
 func View() *gtk.ScrolledWindow {
+	var flds flds
+
+	var fromFlds *gtk.Box
+	var toFlds *gtk.Box
+	var ccFlds *gtk.Box
+	var subjFlds *gtk.Box
+
+	fromFlds, flds.from = fromCtrls()
+	toFlds, flds.to = toCtrls()
+	ccFlds, flds.cc = ccCtrls()
+	subjFlds, flds.subj = subjCtrls()
+	flds.body = bodyCtrls()
 
 	ctrls := errs.Must(gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0))
-
-	fromFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(fromFlds, false, false, 2)
-
-	fromLbl := errs.Must(gtk.LabelNew("From:"))
-	fromLbl.SetWidthChars(8)
-	fromLbl.SetXAlign(float64(gtk.ALIGN_START))
-	fromFlds.PackStart(fromLbl, false, true, 0)
-
-	from := errs.Must(gtk.EntryNew())
-	fromFlds.PackStart(from, true, true, 2)
-
-	// to
-	toFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(toFlds, false, false, 2)
-
-	toLbl := errs.Must(gtk.LabelNew("To:"))
-	toLbl.SetWidthChars(8)
-	toLbl.SetXAlign(float64(gtk.ALIGN_START))
-	toFlds.PackStart(toLbl, false, true, 0)
-
-	to := errs.Must(gtk.EntryNew())
-	toFlds.PackStart(to, true, true, 2)
-
-	//cc
-	ccFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(ccFlds, false, false, 2)
-
-	ccLbl := errs.Must(gtk.LabelNew("CC:"))
-	ccLbl.SetWidthChars(8)
-	ccLbl.SetXAlign(float64(gtk.ALIGN_START))
-	ccFlds.PackStart(ccLbl, false, true, 0)
-
-	cc := errs.Must(gtk.EntryNew())
-	ccFlds.PackStart(cc, true, true, 2)
-
-	// subject
-	subjFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(subjFlds, false, false, 2)
+	ctrls.PackStart(flds.body, true, true, 2)
 
-	subjLbl := errs.Must(gtk.LabelNew("Subject:"))
-	subjLbl.SetWidthChars(8)
-	subjLbl.SetXAlign(float64(gtk.ALIGN_START))
-	subjFlds.PackStart(subjLbl, false, true, 0)
+	scroll := errs.Must(gtk.ScrolledWindowNew(nil, nil))
+	scroll.Add(ctrls)
 
-	subj := errs.Must(gtk.EntryNew())
-	subjFlds.PackStart(subj, true, true, 2)
+	go setFieldsOnAction(
+		app.ListenAction[MsgSetAll](),
+		flds,
+	)
 
-	//body
+	return scroll
+}
+
+func setFieldsOnAction(ch chan MsgSetAll, flds flds) {
+	for setMsg := range ch {
+		setMsg := setMsg
+		glib.IdleAdd(func() bool {
+			b, err := flds.body.GetBuffer()
+			if err != nil {
+				panic(err)
+			}
+			b.SetText(setMsg.Body)
+			flds.subj.SetText(setMsg.Subject)
+			flds.from.SetText(setMsg.From)
+			flds.to.SetText(setMsg.To)
+			flds.cc.SetText(setMsg.CC)
+			return false
+		})
+	}
+}
+
+func bodyCtrls() *gtk.TextView {
 	text := errs.Must(gtk.TextViewNew())
 	text.SetMarginBottom(10)
 	text.SetMarginTop(10)
 	text.SetMarginStart(10)
 	text.SetMarginEnd(10)
-	ctrls.PackStart(text, true, true, 2)
+	return text
+}
 
-	scroll := errs.Must(gtk.ScrolledWindowNew(nil, nil))
-	scroll.Add(ctrls)
-	ch := app.ListenAction[MsgSetAll]()
+func subjCtrls() (*gtk.Box, *gtk.Entry) {
+	subjFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
+	subjLbl := errs.Must(gtk.LabelNew("Subject:"))
+	subjLbl.SetWidthChars(8)
+	subjLbl.SetXAlign(float64(gtk.ALIGN_START))
+	subjFlds.PackStart(subjLbl, false, true, 0)
+	subj := errs.Must(gtk.EntryNew())
+	subjFlds.PackStart(subj, true, true, 2)
+	return subjFlds, subj
+}
 
-	go func() {
-		for setMsg := range ch {
-			setMsg := setMsg
-			glib.IdleAdd(func() bool {
-				b, err := text.GetBuffer()
-				if err != nil {
-					panic(err)
-				}
-				b.SetText(setMsg.Body)
-				subj.SetText(setMsg.Subject)
-				from.SetText(setMsg.From)
-				to.SetText(setMsg.To)
-				cc.SetText(setMsg.CC)
-				return false
-			})
-		}
-	}()
-	return scroll
+func ccCtrls() (*gtk.Box, *gtk.Entry) {
+	ccFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
+	ccLbl := errs.Must(gtk.LabelNew("CC:"))
+	ccLbl.SetWidthChars(8)
+	ccLbl.SetXAlign(float64(gtk.ALIGN_START))
+	ccFlds.PackStart(ccLbl, false, true, 0)
+	cc := errs.Must(gtk.EntryNew())
+	ccFlds.PackStart(cc, true, true, 2)
+	return ccFlds, cc
+}
+
+func toCtrls() (*gtk.Box, *gtk.Entry) {
+	toFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
+	toLbl := errs.Must(gtk.LabelNew("To:"))
+	toLbl.SetWidthChars(8)
+	toLbl.SetXAlign(float64(gtk.ALIGN_START))
+	toFlds.PackStart(toLbl, false, true, 0)
+	to := errs.Must(gtk.EntryNew())
+	toFlds.PackStart(to, true, true, 2)
+	return toFlds, to
+}
+
+func fromCtrls() (*gtk.Box, *gtk.Entry) {
+	fromFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
+	fromLbl := errs.Must(gtk.LabelNew("From:"))
+	fromLbl.SetWidthChars(8)
+	fromLbl.SetXAlign(float64(gtk.ALIGN_START))
+	fromFlds.PackStart(fromLbl, false, true, 0)
+	from := errs.Must(gtk.EntryNew())
+	fromFlds.PackStart(from, true, true, 2)
+	return fromFlds, from
 }
