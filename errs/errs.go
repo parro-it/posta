58ea0ad2bf06df12e912603937f55ac1d812e56c
Andrea Parodi <andrea@parro.it>
2022-01-21 16:14:58 +0100

    tweaks

---
+++ errs/errs.go
@@ -0,0 +1,8 @@
+package errs
+
+func Must[T any](v T, err error) T {
+	if err == nil {
+		return v
+	}
+	panic(err)
+}
--- msgbody/view.go
+++ msgbody/view.go
@@ -1,121 +1,73 @@
 package msgbody
 
 import (
-	"log"
-
 	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/parro-it/posta/app"
+	"github.com/parro-it/posta/errs"
 )
 
 // Creates a tree view and the tree store that holds its data
 func View() *gtk.ScrolledWindow {
 
-	ctrls, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// from
-	fromFlds, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
+	ctrls := errs.Must(gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0))
 
-	fromLbl, err := gtk.LabelNew("From:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fromLbl.SetWidthChars(8)
-	fromLbl.SetXAlign(float64(gtk.ALIGN_START))
-
-	fromFlds.PackStart(fromLbl, false, true, 0)
-
-	from, err := gtk.EntryNew()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fromFlds.PackStart(from, true, true, 2)
+	fromFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(fromFlds, false, false, 2)
 
+	fromLbl := errs.Must(gtk.LabelNew("From:"))
+	fromLbl.SetWidthChars(8)
+	fromLbl.SetXAlign(float64(gtk.ALIGN_START))
+	fromFlds.PackStart(fromLbl, false, true, 0)
+
+	from := errs.Must(gtk.EntryNew())
+	fromFlds.PackStart(from, true, true, 2)
+
 	// to
-	toFlds, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	toLbl, err := gtk.LabelNew("To:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	toLbl.SetWidthChars(8)
-	toLbl.SetXAlign(float64(gtk.ALIGN_START))
-
-	toFlds.PackStart(toLbl, false, true, 0)
-
-	to, err := gtk.EntryNew()
-	if err != nil {
-		log.Fatal(err)
-	}
-	toFlds.PackStart(to, true, true, 2)
+	toFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(toFlds, false, false, 2)
 
+	toLbl := errs.Must(gtk.LabelNew("To:"))
+	toLbl.SetWidthChars(8)
+	toLbl.SetXAlign(float64(gtk.ALIGN_START))
+	toFlds.PackStart(toLbl, false, true, 0)
+
+	to := errs.Must(gtk.EntryNew())
+	toFlds.PackStart(to, true, true, 2)
+
 	//cc
-	ccFlds, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	ccLbl, err := gtk.LabelNew("CC:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ccLbl.SetWidthChars(8)
-	ccLbl.SetXAlign(float64(gtk.ALIGN_START))
-
-	ccFlds.PackStart(ccLbl, false, true, 0)
-
-	cc, err := gtk.EntryNew()
-	if err != nil {
-		log.Fatal(err)
-	}
-	ccFlds.PackStart(cc, true, true, 2)
+	ccFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(ccFlds, false, false, 2)
 
+	ccLbl := errs.Must(gtk.LabelNew("CC:"))
+	ccLbl.SetWidthChars(8)
+	ccLbl.SetXAlign(float64(gtk.ALIGN_START))
+	ccFlds.PackStart(ccLbl, false, true, 0)
+
+	cc := errs.Must(gtk.EntryNew())
+	ccFlds.PackStart(cc, true, true, 2)
+
 	// subject
-	subjFlds, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	subjLbl, err := gtk.LabelNew("Subject:")
-	if err != nil {
-		log.Fatal(err)
-	}
-	subjLbl.SetWidthChars(8)
-	subjLbl.SetXAlign(float64(gtk.ALIGN_START))
-
-	subjFlds.PackStart(subjLbl, false, true, 0)
-
-	subj, err := gtk.EntryNew()
-	if err != nil {
-		log.Fatal(err)
-	}
-	subjFlds.PackStart(subj, true, true, 2)
+	subjFlds := errs.Must(gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0))
 	ctrls.PackStart(subjFlds, false, false, 2)
 
+	subjLbl := errs.Must(gtk.LabelNew("Subject:"))
+	subjLbl.SetWidthChars(8)
+	subjLbl.SetXAlign(float64(gtk.ALIGN_START))
+	subjFlds.PackStart(subjLbl, false, true, 0)
+
+	subj := errs.Must(gtk.EntryNew())
+	subjFlds.PackStart(subj, true, true, 2)
+
 	//body
-	text, err := gtk.TextViewNew()
-	if err != nil {
-		log.Fatal("Unable to create textview:", err)
-	}
+	text := errs.Must(gtk.TextViewNew())
 	text.SetMarginBottom(10)
 	text.SetMarginTop(10)
 	text.SetMarginStart(10)
 	text.SetMarginEnd(10)
 	ctrls.PackStart(text, true, true, 2)
 
-	scroll, err := gtk.ScrolledWindowNew(nil, nil)
-	if err != nil {
-		log.Fatal("Unable to create scroolbox:", err)
-	}
+	scroll := errs.Must(gtk.ScrolledWindowNew(nil, nil))
 	scroll.Add(ctrls)
 	ch := app.ListenAction[MsgSetAll]()
 
